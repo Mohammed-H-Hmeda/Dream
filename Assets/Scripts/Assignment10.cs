94d1ea1efa901c5b91948418f53447094a5779a1
Mohammed H Hemeda <baster2004w@gmail.com>
2024-10-31 21:14:07 +0200

    ...

--- Assets/Scripts/Assignment10.cs
+++ Assets/Scripts/Assignment10.cs
@@ -7,19 +7,18 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-    int num=1;
-     for(;num<11;num+=1)
-     {
-        
-        Debug.Log(num+"x"+"5"+'='+Multiply(5,num));
-
-        
-     }
-     int Multiply(int num1, int num2)
+    //Define the Multiply Function:
+    int Multiply(int num1, int num2)
      {
         int mult = num1*num2;
         return mult;
      }   
+    int num=1;
+    //In the Start method, create a for loop
+     for(;num<11;num+=1)
+     {
+        Debug.Log(num+"x"+"5"+'='+Multiply(5,num));
+     }
     }
 
     // Update is called once per frame
