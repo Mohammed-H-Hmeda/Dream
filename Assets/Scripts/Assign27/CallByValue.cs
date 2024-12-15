f1f112bd208dcf4575cae9623f41ec5d98601f04
Mohammed H Hemeda <baster2004w@gmail.com>
2024-12-15 16:20:06 +0200

    some new change

---
+++ Assets/Scripts/Assign27/CallByValue.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+namespace assign27{
+public class CallByValue : MonoBehaviour
+{
+    public void addition(int num)
+    {
+        num+=10;
+    }
+    public void refaddition(ref int num)
+    {
+        num+=10;
+    }
+    public void outaddition(out int num1)
+    {
+        num1=25;
+    }
+    // Start is called before the first frame update
+    void Start()
+    { 
+        int num1=5;
+        int num2=10;
+        addition(num1);
+        Debug.Log("Calling by value "+num1);
+        refaddition(ref num2);
+        Debug.Log("Calling by refernce "+ num2);
+        outaddition(out num1);
+        Debug.Log("Calling by out "+ num1);
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+}
